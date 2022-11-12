9f5259ff141687c88934e7c541c6751f3eaa825e
BouSpace <86728767+BouSpace@users.noreply.github.com>
2022-11-12 13:50:49 +0000

    teamWork2

--- serviceachat/achat/serializers.py
+++ serviceachat/achat/serializers.py
@@ -12,25 +12,25 @@ class articleSerializer(serializers.ModelSerializer):
         model = Article
         fields = '__all__'
 
-#class lignAchatSerializer(serializers.ModelSerializer):
-#    class Meta:
-#        depth = 1
-#        model = LigneAchat
-#        fields = '__all__'
-
 class lignAchatSerializer(serializers.ModelSerializer):
     class Meta:
+        depth = 0
         model = LigneAchat
-        fields = '__all__' #['id','prixUnitaire','quantite']
+        fields = '__all__'
 
-#class panierSerializer(serializers.ModelSerializer):
+#class lignAchatSerializer(serializers.ModelSerializer):
 #    class Meta:
-#        model = Panier
-#        fields = '__all__'
+#        model = LigneAchat
+#        fields = '__all__' #['id','prixUnitaire','quantite']
 
 class panierSerializer(serializers.ModelSerializer):
-    panier = lignAchatSerializer(many=True)
-
     class Meta:
         model = Panier
         fields = '__all__'
+
+#class panierSerializer(serializers.ModelSerializer):
+#    panier = lignAchatSerializer(many=True)
+
+#    class Meta:
+#        model = Panier
+#        fields = '__all__'
